0a4142671e065048c2a00c2a55bc187ad48ae681
arifraihan188 <arifraihan188@gmail.com>
2023-12-24 22:59:41 +1100

    First Commit
    First Date: 24-12-23

---
+++ src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <main className='About'>
+            <h2>About</h2>
+            <p style={{ marginTop: "1rem" }}>This blog app is a project of creating blog features to enable authors to publish articles, opinions or product reviews as posts.</p>
+        </main>
+    )
+}
+
+export default About
